599f4c67c436930823bf4691b16d88eba99f3ad9
rondayvoo <69455258+rondayvoo@users.noreply.github.com>
2021-10-08 16:54:32 +0800

    Add utility functions, build framework

--- MyProject/main.c
+++ MyProject/main.c
@@ -1,29 +1,247 @@
-/*----------------------------------------------------------------------------
- * CMSIS-RTOS 'main' function template
- *---------------------------------------------------------------------------*/
- 
 #include "RTE_Components.h"
-#include  CMSIS_device_header
+#include CMSIS_device_header
 #include "cmsis_os2.h"
+#include "stdbool.h"
 
-#define RED_LED 18
-#define GREEN_LED 19
-#define BLUE_LED 1
-#define FRONT_LEFT_MOTOR 0
-#define FRONT_RIGHT_MOTOR 0
-#define BACK_LEFT_MOTOR 0
-#define BACK_RIGHT_MOTOR 0
 #define MASK(x) (1 << x)
- 
+
+#define RED_LED 18					//PTB18
+#define GREEN_LED 19				//PTB19
+#define BLUE_LED 1					//PTD1
+
+#define LEFT_MOTOR_FWD 0 		//PTB0 - TPM1_CH0
+#define LEFT_MOTOR_RVS 1 		//PTB1 - TPM1_CH1
+#define RIGHT_MOTOR_FWD 2 	//PTB2 - TPM2_CH0
+#define RIGHT_MOTOR_RVS 3 	//PTB3 - TPM2_CH1
+
+#define BUZZER 0
+
+#define BAUD_RATE 9600
+#define UART_TX 22					//PTE22
+#define UART_RX 23					//PTE23
+#define UART2_INT_PRIO 128
+
+#define Q_SIZE 32
+
+typedef struct {
+	unsigned char Data[Q_SIZE];
+	unsigned int Head; 				// points to oldest data element
+	unsigned int Tail; 				// points to next free space
+	unsigned int Size; 				// quantity of elements in queue
+} Q_T;
+
+Q_T Tx_Q, Rx_Q;
+
 /*----------------------------------------------------------------------------
- * Application main thread
+ * LEDs
  *---------------------------------------------------------------------------*/
 
+void activateLED(int input, bool turnOn)
+{
+	switch (input)
+	{
+		case 0:
+			if (turnOn)
+				PTB->PDDR |= MASK(RED_LED);
+			else
+				PTB->PDDR &= ~MASK(RED_LED);
+			break;
+		case 1:
+			if (turnOn)
+				PTB->PDDR |= MASK(GREEN_LED);
+			else
+				PTB->PDDR &= ~MASK(GREEN_LED);
+			break;
+		case 2:
+			if (turnOn)
+				PTD->PDDR |= MASK(BLUE_LED);
+			else
+				PTD->PDDR &= ~MASK(BLUE_LED);
+			break;
+		default:
+			break;
+	}
+}
+
+/*----------------------------------------------------------------------------
+ * Movement
+ *---------------------------------------------------------------------------*/
+
+void moveStop()
+{
+	TPM1_C0V = 0;
+	TPM1_C1V = 0;
+	TPM2_C0V = 0;
+	TPM2_C1V = 0;
+}
+
+void moveForward(int power)
+{
+	if (power > 100)
+		power = 100;
+	if (power < 0)
+		power = 0;
+	
+	float coeff = (float) power / 100.0f;
+	
+	TPM1_C0V = TPM1->MOD * coeff;
+	TPM1_C1V = 0;
+	TPM2_C0V = TPM2->MOD * coeff;
+	TPM2_C1V = 0;
+}
+
+void moveBackward(int power)
+{
+	if (power > 100)
+		power = 100;
+	if (power < 0)
+		power = 0;
+	
+	float coeff = (float) power / 100.0f;
+	
+	TPM1_C0V = 0;
+	TPM1_C1V = TPM1->MOD * coeff;
+	TPM2_C0V = 0;
+	TPM2_C1V = TPM2->MOD * coeff;
+}
+
+void moveLeft(int power)
+{
+	if (power > 100)
+		power = 100;
+	if (power < 0)
+		power = 0;
+	
+	float coeff = (float) power / 100.0f;
+	
+	TPM1_C0V = 0;
+	TPM1_C1V = TPM1->MOD * coeff;
+	TPM2_C0V = TPM2->MOD * coeff;
+	TPM2_C1V = 0;
+}
+
+void moveRight(int power)
+{
+	if (power > 100)
+		power = 100;
+	if (power < 0)
+		power = 0;
+	
+	float coeff = (float) power / 100.0f;
+	
+	TPM1_C0V = TPM1->MOD * coeff;
+	TPM1_C1V = 0;
+	TPM2_C0V = 0;
+	TPM2_C1V = TPM2->MOD * coeff;
+}
+
+/*----------------------------------------------------------------------------
+ * Queue Operations
+ *---------------------------------------------------------------------------*/
+
+void Q_Init(Q_T * q) 
+{
+	unsigned int i;
+	
+	for (i=0; i<Q_SIZE; i++)
+		q->Data[i] = 0; // to simplify our lives when debugging
+	
+	q->Head = 0;
+	q->Tail = 0;
+	q->Size = 0;
+}
+
+int Q_Empty(Q_T * q) 
+{
+	return q->Size == 0;
+}
+
+int Q_Full(Q_T * q) 
+{
+	return q->Size == Q_SIZE;
+}
+
+int Q_Enqueue(Q_T * q, unsigned char d) 
+{
+	// What if queue is full?
+	if (!Q_Full(q)) 
+	{
+		q->Data[q->Tail++] = d;
+		q->Tail %= Q_SIZE;
+		q->Size++;
+		return 1; // success
+	} 
+	
+	else
+		return 0; // failure
+}
+unsigned char Q_Dequeue(Q_T * q) 
+{
+	// Must check to see if queue is empty before dequeueing
+	unsigned char t = 0;
+	
+	if (!Q_Empty(q)) 
+	{
+		t = q->Data[q->Head];
+		q->Data[q->Head++] = 0; // to simplify debugging
+		q->Head %= Q_SIZE;
+		q->Size--;
+	}
+	
+	return t;
+}
+
+/*----------------------------------------------------------------------------
+ * UART
+ *---------------------------------------------------------------------------*/
+
+void UART2_IRQHandler(void) 
+{
+	NVIC_ClearPendingIRQ(UART2_IRQn);
+	
+	//IRQ Transmitter
+	if (UART2->S1 & UART_S1_TDRE_MASK) 
+	{
+		// can send another character
+		if (!Q_Empty(&Tx_Q)) 
+		{
+			UART2->D = Q_Dequeue(&Tx_Q);
+		} 
+		
+		else 
+		{
+			// queue is empty so disable tx
+			UART2->C2 &= ~UART_C2_TIE_MASK;
+		}
+	}
+	
+	//IRQ Reciever
+	if (UART2->S1 & UART_S1_RDRF_MASK) 
+	{
+		// received a character
+		if (!Q_Full(&Rx_Q)) 
+		{
+			Q_Enqueue(&Rx_Q, UART2->D);
+		} 
+		
+		else 
+		{
+			// error - queue full.
+			while (1);
+		}
+	}
+}
+
+/*----------------------------------------------------------------------------
+ * Initialization
+ *---------------------------------------------------------------------------*/
 
 void initLED()
 {
+	// Enable Clock to PORTB and PORTD
 	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTD_MASK));
 	
+	// Configure MUX settings to make all 3 pins GPIO
 	PORTB->PCR[RED_LED] &= ~PORT_PCR_MUX_MASK;
 	PORTB->PCR[RED_LED] |= PORT_PCR_MUX(1);
 	
@@ -40,22 +258,34 @@ void initMotors()
 	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
 	
 	//Configure to be mode 3: TPM1_CH0
-	PORTB->PCR[FRONT_LEFT_MOTOR] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[FRONT_LEFT_MOTOR] |= PORT_PCR_MUX(3);
+	PORTB->PCR[LEFT_MOTOR_FWD] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[LEFT_MOTOR_FWD] |= PORT_PCR_MUX(3);
 	
 	//Configure to be mode 3: TPM1_CH1
-	PORTB->PCR[FRONT_RIGHT_MOTOR] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[FRONT_RIGHT_MOTOR] |= PORT_PCR_MUX(3);
+	PORTB->PCR[LEFT_MOTOR_RVS] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[LEFT_MOTOR_RVS] |= PORT_PCR_MUX(3);
+	
+	//Configure to be mode 3: TPM2_CH0
+	PORTB->PCR[RIGHT_MOTOR_FWD] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[RIGHT_MOTOR_FWD] |= PORT_PCR_MUX(3);
+	
+	//Configure to be mode 3: TPM2_CH1
+	PORTB->PCR[RIGHT_MOTOR_RVS] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[RIGHT_MOTOR_RVS] |= PORT_PCR_MUX(3);
 	
 	//Enable clock for TPM1 module
 	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
 	
-	//Select MCGFLLCLK
+	//Enable clock for TPM2 module
+	SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK;
+	
+	//Select MCGFLLCLK for system
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
 	
-	//Set to 50Hz
+	//Set both timers to 50Hz
 	TPM1->MOD = 7500;
+	TPM2->MOD = 7500;
 	
 	//Set LPTPM counter to increment on every counter clock
 	//Set prescaler to 1/128
@@ -64,20 +294,77 @@ void initMotors()
 	TPM1->SC |= ((TPM_SC_CMOD(1)) | (TPM_SC_PS(7)));
 	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
 	
+	TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM2->SC |= ((TPM_SC_CMOD(1)) | (TPM_SC_PS(7)));
+	TPM2->SC &= ~(TPM_SC_CPWMS_MASK);
+	
 	//Select edge-aligned PWM with high-true pulses
 	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM1_C0SC |= (TPM_CnSC_ELSB(1)) | (TPM_CnSC_MSB(1));
+	
+	TPM2_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM2_C0SC |= (TPM_CnSC_ELSB(1)) | (TPM_CnSC_MSB(1));
 }
 
 void initBuzzer()
 {
 }
 
+void initUART2(uint32_t baud_rate) {
+	uint32_t divisor, bus_clock;
+	
+	//Enable clock for UART2 and Port E
+	SIM->SCGC4 |= SIM_SCGC4_UART2_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	
+	//Configure mode 4 for Port E Pin 22: UART2_TX
+	PORTE->PCR[UART_TX] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[UART_RX] |= PORT_PCR_MUX(4);
+	
+	//Configure mode 4 for Port E Pin 23: UART2_RX
+	PORTE->PCR[UART_TX] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[UART_RX] |= PORT_PCR_MUX(4);
+	
+	//Turn off RX and TX, disable UART
+	UART2->C2 &= ~((UART_C2_TE_MASK) | (UART_C2_RE_MASK));
+	
+	//Update the baud setting
+	//Note: Always write to BDH first, then BDL
+	bus_clock = DEFAULT_SYSTEM_CLOCK / 2;
+	divisor = bus_clock / (baud_rate * 16);
+	UART2->BDH = UART_BDH_SBR(divisor >> 8);
+	UART2->BDL = UART_BDH_SBR(divisor);
+	
+	//Use default UART2 configuration
+	UART2->C1 = 0;
+	UART2->S2 = 0;
+	UART2->C3 = 0;
+	
+	//Turn on TX and RX, enable UART
+	UART2->C2 |= ((UART_C2_TE_MASK) | (UART_C2_RE_MASK));
+	
+	NVIC_SetPriority(UART2_IRQn, 128);
+	NVIC_ClearPendingIRQ(UART2_IRQn);
+	NVIC_EnableIRQ(UART2_IRQn);
+	UART2->C2 |= (UART_C2_TIE_MASK | UART_C2_RIE_MASK);
+	UART2->C2 |= UART_C2_RIE_MASK;
+	Q_Init(&Tx_Q);
+	Q_Init(&Rx_Q);
+}
+
+/*----------------------------------------------------------------------------
+ * Threads
+ *---------------------------------------------------------------------------*/
+
 void app_main (void *argument) {
  
   // ...
   for (;;) {}
 }
+
+/*----------------------------------------------------------------------------
+ * Main
+ *---------------------------------------------------------------------------*/
  
 int main (void) {
  
@@ -85,6 +372,8 @@ int main (void) {
   SystemCoreClockUpdate();
 	initLED();
   initMotors();
+	initBuzzer();
+	initUART2(BAUD_RATE);
  
   osKernelInitialize();                 // Initialize CMSIS-RTOS
   osThreadNew(app_main, NULL, NULL);    // Create application main thread
